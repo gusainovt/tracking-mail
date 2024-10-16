632d579b9b11028e3240f609080a566125f88f8c
Timur-Gus <timurka24478@gmail.ru>
2024-10-16 15:02:55 +0300

    Initial commit

---
+++ src/main/java/com/example/trackingmail/TrackingMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackingmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trackingmail/TrackingMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackingmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
